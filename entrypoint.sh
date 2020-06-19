743208bf6608bde9cf7692e0f86376da792cd863
Mitch Talmadge <mitch@mitchtalmadge.com>
2020-06-18 20:30:44 -0600

    #23: Improve user creation & fix home directory permissions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,26 @@
 #!/bin/bash -e
 set +o xtrace
 
-# Create User
-if [ ! "$(getent group amp)" ]
-then
-	groupadd \
-		--gid "${GID}" \
-		-r \
-		amp
-	useradd \
-		--uid "${UID}" \
-		--gid "${GID}" \
-		--no-log-init \
-		--shell /bin/bash \
-		-r \
-		-m \
+# Create user and group that will own the config files (if they don't exist already).
+if [ ! "$(getent group ${GID})" ]; then
+  # Create group
+	addgroup \
+		--gid ${GID} \
 		amp
 fi
+APP_GROUP=`getent group ${GID} | awk -F ":" '{ print $1 }'`
+if [ ! "$(getent passwd ${UID})" ]; then
+  # Create user
+  adduser \
+		--uid ${UID} \
+		--shell /bin/bash \
+		--no-create-home \
+		--ingroup ${APP_GROUP} \
+    --system \
+		amp
+fi
+APP_USER=`getent passwd ${UID} | awk -F ":" '{ print $1 }'`
+chown -R ${APP_USER}:${APP_GROUP} /home/amp
 
 # Set up environment
 cd /home/amp
@@ -24,25 +28,25 @@ cd /home/amp
 # Create Main Instance
 if [ ! -d ".ampdata/instances/Main" ]
 then
-	su amp --command "ampinstmgr CreateInstance \"${MODULE}\" Main 0.0.0.0 \"${PORT}\" \"${LICENCE}\" \"${USERNAME}\" \"${PASSWORD}\""
+	su ${APP_USER} --command "ampinstmgr CreateInstance \"${MODULE}\" Main 0.0.0.0 \"${PORT}\" \"${LICENCE}\" \"${USERNAME}\" \"${PASSWORD}\""
 else
   # Automatic AMP Upgrades
-  su amp --command "ampinstmgr UpgradeAll"
+  su ${APP_USER} --command "ampinstmgr UpgradeAll"
 fi
 
 # Make Main instance start on boot if disabled.
 echo "Ensuring Main Instance will Start on Boot..."
-su amp --command "ampinstmgr ShowInstanceInfo Main | grep \"Start on Boot\" | grep \"No\" && ampinstmgr SetStartBoot Main || true"
+su ${APP_USER} --command "ampinstmgr ShowInstanceInfo Main | grep \"Start on Boot\" | grep \"No\" && ampinstmgr SetStartBoot Main || true"
 
 # Startup
 echo "Starting AMP..."
-su amp --command "ampinstmgr StartBoot"
+su ${APP_USER} --command "ampinstmgr StartBoot"
 echo "AMP Started."
 
 # Trap SIGTERM for a graceful shutdown
 shutdown() {
   echo "Shutting Down AMP..."
-  su amp --command "ampinstmgr StopAll"
+  su ${APP_USER} --command "ampinstmgr StopAll"
   echo "Shutdown Complete."
   exit 0
 }
